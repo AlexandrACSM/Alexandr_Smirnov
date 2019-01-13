6c58ddc9c5e38ba572c9ac507bbc256e801606dc
Alexandrsmi <alexandrsmi02@mail.ru>
2019-01-13 21:56:04 +1100

    update'

--- chap_007/src/main/java/ru/job4j/wait/ParallelSearch.java
+++ chap_007/src/main/java/ru/job4j/wait/ParallelSearch.java
@@ -2,12 +2,14 @@ package ru.job4j.wait;
 
 public class ParallelSearch {
 
-    public static void main(String[] args)  {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+    public static void main(String[] args) {
+//        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
 //        final Thread consumer = new Thread(
 //                () -> {
 //                    while (true){
 //                        try{
+////
+////
 //                            System.out.println(queue.poll());
 //                        }
 //                         catch (InterruptedException e){
@@ -18,18 +20,19 @@ public class ParallelSearch {
 //                }
 //        );
 //        consumer.start();
-        new Thread(
-                () ->{
-                    for(int i = 0; i!=3;i++){
-                        queue.offer(i);
-                        try {
-                            Thread.sleep(500);
-                        }catch (InterruptedException e){
-                           e.printStackTrace();
-                        }
-                    }
-
-                }
-        ).start();
+//        new Thread(
+//                () ->{
+//                    for(int i = 0; i!=3;i++){
+//                        queue.offer(i);
+//                        try {
+//                            Thread.sleep(500);
+//                        }catch (InterruptedException e){
+//                           e.printStackTrace();
+//                        }
+//                    }
+//
+//                }
+//        ).start();
+//    }
     }
 }
--- chap_007/src/main/java/ru/job4j/wait/ProducerCustomer.java
+++ chap_007/src/main/java/ru/job4j/wait/ProducerCustomer.java
@@ -4,11 +4,44 @@ public class ProducerCustomer {
     private final Object lock = new Object();
     private boolean condition = true;
 
-    public void doSomething(){
+    public void doSomething() throws InterruptedException {
+        synchronized (this.lock) {
+            while (this.condition) {
+                System.out.println(String.format("%s wait",Thread.currentThread().getId()));
+                lock.wait();
+            }
+            System.out.println(String.format("%s userfull work", Thread.currentThread().getId()));
+        }
+    }
 
+    public void changeBlock(final boolean enable) {
+        synchronized (this.lock) {
+            System.out.println(String.format("% enable", Thread.currentThread().getId()));
+            this.condition = enable;
+            this.lock.notify();
+        }
     }
 
     public static void main(String[] args) {
+        ProducerCustomer blockingWork = new ProducerCustomer();
+        Thread producer = new Thread() {
+            @Override
+            public void run() {
+                blockingWork.changeBlock(false);
+            }
+        };
 
+        Thread customer = new Thread(){
+            @Override
+            public void run (){
+                try{
+                    blockingWork.doSomething();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        producer.start();
+        customer.start();
     }
 }
